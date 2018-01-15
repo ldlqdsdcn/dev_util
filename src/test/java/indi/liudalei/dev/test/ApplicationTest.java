aa2d2312536639ac5ca1d6baa07302d5feff5dfe
liudalei <liudalei@shanshoufu.com>
2018-01-15 08:54:20 +0800

    提交部分代码

---
+++ src/test/java/indi/liudalei/dev/test/ApplicationTest.java
@@ -0,0 +1,16 @@
+package indi.liudalei.dev.test;
+
+import indi.liudalei.dev.Application;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author liudalei
+ * @version 1.0.0
+ * @date 2018/1/15
+ * @description
+ */
+public class ApplicationTest {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
