dc40ff0ff68ef40f8c2679ab61eaee0982e44180
liudalei <liudalei@shanshoufu.com>
2018-01-15 14:30:20 +0800

    代码重构，添加utf8类库

--- src/main/webapp/encryption/base64/base64.js
+++ src/main/webapp/encryption/base64/base64.js
@@ -1,9 +1,20 @@
 app.controller('base64Ctrl', function ($scope) {
+
+    require(
+        { 'paths': { 'base64': 'base64'}
+        }, ['base64'],
+        function(base64) {
+            $scope.base64 = base64;
+        }
+    );
+   /* require(["base64"], function (base64) {
+        $scope.base64 = base64;
+    });*/
     $scope.code = {};
     $scope.encode = function () {
-        $scope.code.outputText = Base64.encode($scope.code.inputText);
+        $scope.code.outputText = $scope.base64.encode($scope.code.inputText);
     }
     $scope.decode = function () {
-        $scope.code.outputText = Base64.decode($scope.code.inputText);
+        $scope.code.outputText = $scope.base64.decode($scope.code.inputText);
     }
 });
