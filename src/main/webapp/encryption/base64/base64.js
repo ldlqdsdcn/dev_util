8ba26d3bbff14997fd2e80a53a39d1ff03e44540
liudalei <liudalei@shanshoufu.com>
2018-01-31 09:38:33 +0800

    提交部分代码

--- src/main/webapp/encryption/base64/base64.js
+++ src/main/webapp/encryption/base64/base64.js
@@ -1,31 +1,20 @@
 app.controller('base64Ctrl', function ($scope) {
 
     require(
-        { 'paths': { 'base64': 'base64'}
-        }, ['base64'],
-        function(base64) {
+        {
+            'paths': {'base64': 'base64', 'utf8': 'utf8'}
+        }, ['base64', 'utf8'],
+        function (base64, utf8) {
             $scope.base64 = base64;
         }
     );
-    require(
-        { 'paths': { 'utf8': 'utf8'}
-        }, ['utf8'],
-        function(utf8) {
-            $scope.utf8 = utf8;
-        }
-    );
-   /* require(["base64"], function (base64) {
-        $scope.base64 = base64;
-    });*/
     $scope.code = {};
     $scope.encode = function () {
-        var hexValue=$scope.utf8.encode($scope.code.inputText);
+        var hexValue = utf8.encode($scope.code.inputText);
         $scope.code.outputText = $scope.base64.encode(hexValue);
     }
     $scope.decode = function () {
-
-
-     var hexValue  = $scope.base64.decode($scope.code.inputText);
-        $scope.code.outputText=$scope.utf8.decode(hexValue);
+        var hexValue = $scope.base64.decode($scope.code.inputText);
+        $scope.code.outputText = utf8.decode(hexValue);
     }
 });
